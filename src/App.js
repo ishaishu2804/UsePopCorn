4c0fc6d629e1b59ec09bcc8458c401c6cb7d1278
Isha Nagaraj <127282387+ishaishu2804@users.noreply.github.com>
2025-09-26 18:10:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -276,7 +276,7 @@ function SelectedMovie({ selectedId, onCloseId, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
